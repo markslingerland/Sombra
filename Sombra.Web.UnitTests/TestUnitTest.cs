093328a1af884440daa63acd73e2f9300acd5db0
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-12 17:35:40 +0100

    Add sample unittest

---
+++ Sombra.Web.UnitTests/TestUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sombra.Web.UnitTests
+{
+    [TestClass]
+    public class TestUnitTest
+    {
+        [TestMethod]
+        public void This_Test_Will_Succeed()
+        {
+            var test = new TestClass();
+            Assert.IsTrue(test.IsTrue);
+        }
+    }
+}
---
+++ Sombra.Web/TestClass.cs
@@ -0,0 +1,7 @@
+﻿namespace Sombra.Web
+{
+    public class TestClass
+    {
+        public bool IsTrue => true;
+    }
+}

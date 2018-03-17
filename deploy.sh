9974085a90e6d542ef058dabfdfd1e4b17380270
markslingerland <markslingerland@hotmail.com>
2018-03-17 15:38:37 +0100

    - Forgot to change the exact path

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+chmod 600 rsa # Allow read access to the private key
+ssh-add rsa # Add the private key to SSH
 
 git config --global push.default matching
 git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR

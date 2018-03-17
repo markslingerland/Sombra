24102694c2edef22118efd50ba372e42e03df41c
markslingerland <markslingerland@hotmail.com>
2018-03-17 20:57:08 +0100

    --

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,6 @@ git push deploy master
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh -o "StrictHostKeyChecking no" docker-deployment@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
+  ls
   docker-compose up
 EOF

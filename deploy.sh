538ef89632b8f8b7c02a0d6e38f21b68ae423a99
markslingerland <markslingerland@hotmail.com>
2018-03-17 22:48:27 +0100

    -- narrowing down

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ chmod 600 rsa # Allow read access to the private key
 ssh-add rsa # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
+# git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-# ssh -o "StrictHostKeyChecking no" docker-deployment@$IP -p $PORT <<EOF
-#   cd $DEPLOY_DIR
-#   docker-compose up
-# EOF
+ssh -o "StrictHostKeyChecking no" docker-deployment@$IP -p $PORT <<EOF
+  cd $DEPLOY_DIR
+  docker-compose up
+EOF

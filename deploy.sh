f6cfa1218fdfea51991caca4b127d4e937c146f2
markslingerland <markslingerland@hotmail.com>
2018-03-17 16:13:43 +0100

    - Changed the ssh-user

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.
-ssh apps@$IP -p $PORT <<EOF
+ssh docker-deployment@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   docker-compose up
 EOF

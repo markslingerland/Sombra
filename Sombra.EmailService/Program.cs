3624289e0946428581572fcd5f529a15550fcb69
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-17 19:00:58 +0100

    Add Sombra.EmailService

---
+++ Sombra.EmailService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sombra.EmailService
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("EmailService started..");
+        }
+    }
+}

491ecbf40909578e1198f6e3d6c3125da911b43c
markslingerland <markslingerland@hotmail.com>
2018-03-17 22:17:09 +0100

    -

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ chmod 600 rsa # Allow read access to the private key
 ssh-add rsa # Add the private key to SSH
 
 git config --global push.default matching
-git remote add deploy git@$IP:$PORT$DEPLOY_DIR
+git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.

c5542a8c9435630380f5c0f7afe6256f49975174
markslingerland <markslingerland@hotmail.com>
2018-03-17 22:27:39 +0100

    -

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ chmod 600 rsa # Allow read access to the private key
 ssh-add rsa # Add the private key to SSH
 
 git config --global push.default matching
-ssh-agent bash -c  'git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR'
+ssh-agent $(git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR)
 git push deploy master
 
 # Skip this command if you don't need to execute any additional commands after deploying.

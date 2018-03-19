547d01d308e223c916828d22cfd7639675558c04
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-19 14:12:04 +0100

    Add email messaging infrastructure

---
+++ Sombra.Messaging/Messages/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sombra.Messaging.Messages
+{
+    public class Email : Message
+    {
+        public Email(string sender, string recipient, string subject, string body, bool isHtml = false)
+        {
+            Sender = sender;
+            Recipient = recipient;
+            Subject = subject;
+            Body = body;
+            IsHtml = isHtml;
+        }
+
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public bool IsHtml { get; set; }
+    }
+}

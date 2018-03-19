fc908f782f1ee71735cb882ad86fa07fffc0bb61
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-19 19:04:05 +0100

    Implement message logging

---
+++ Sombra.LoggingService/MessageConsumer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ.AutoSubscribe;
+using MongoDB.Driver;
+using Sombra.Messaging;
+
+namespace Sombra.LoggingService
+{
+    public class MessageConsumer : IConsumeAsync<Message>
+    {
+        private readonly IMongoCollection<LogEntry> _mongoCollection;
+
+        public MessageConsumer(IMongoCollection<LogEntry> mongoCollection)
+        {
+            _mongoCollection = mongoCollection;
+        }
+
+        public async Task Consume(Message message)
+        {
+            await _mongoCollection.InsertOneAsync(new LogEntry(message, DateTime.UtcNow)).ConfigureAwait(false);
+        }
+    }
+}

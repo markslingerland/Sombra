a463278f60786d0806999168a9ac5ce1a4501e71
markslingerland <markslingerland@hotmail.com>
2018-03-19 19:09:47 +0100

    - Added IdentityService project

---
+++ Sombra.IdentityService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sombra.IdentityService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

7e0743780cbc7fb56000f341e6fe495499ed9cb4
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-19 21:54:46 +0100

    Add ExpressionExtensions

---
+++ Sombra.Core/ExpressionDelegateVisitor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Sombra.Core
+{
+    internal sealed class ExpressionDelegateVisitor : ExpressionVisitor
+    {
+        private readonly Func<Expression, Expression> _visitor;
+        private readonly bool _recursive;
+
+        public static Expression Visit(Expression exp, Func<Expression, Expression> visitor, bool recursive)
+        {
+            return new ExpressionDelegateVisitor(visitor, recursive).Visit(exp);
+        }
+
+        private ExpressionDelegateVisitor(Func<Expression, Expression> visitor, bool recursive)
+        {
+            _visitor = visitor ?? throw new ArgumentNullException(nameof(visitor));
+            _recursive = recursive;
+        }
+
+        public override Expression Visit(Expression node)
+        {
+            if (_recursive)
+            {
+                return base.Visit(_visitor(node));
+            }
+            var visited = _visitor(node);
+            if (visited == node) return base.Visit(visited);
+            return visited;
+        }
+
+    }
+}

7ebd685194c75de02443334e9df44af0b8a21ef0
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-19 20:37:31 +0100

    Add IAsyncRequestHandler

---
+++ Sombra.Messaging/IAsyncRequestHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Sombra.Messaging
+{
+    public interface IAsyncRequestHandler<in TRequest, TResponse>
+    {
+        Task<TResponse> Handle(TRequest message);
+    }
+}

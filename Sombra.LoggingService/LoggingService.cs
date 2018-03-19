7858b75b56b352e1531044ec49d241cdd9baade9
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-19 15:36:18 +0100

    Add LoggingService

---
+++ Sombra.LoggingService/LoggingService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using EasyNetQ.AutoSubscribe;
+using Sombra.Messaging;
+
+namespace Sombra.LoggingService
+{
+    public class LoggingService : IConsumeAsync<Message>
+    {
+        public Task Consume(Message message)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Sombra.LoggingService/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using EasyNetQ;
+using EasyNetQ.AutoSubscribe;
+
+namespace Sombra.LoggingService
+{
+    class Program
+    {
+        private static string _subscriptionIdPrefix = "Sombra.LoggingService";
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("LoggingService started..");
+
+            var rabbitMqHost = Environment.GetEnvironmentVariable("RABBITMQ_HOST");
+            var rabbitMqUser = Environment.GetEnvironmentVariable("RABBITMQ_USER");
+            var rabbitMqPassword = Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD");
+
+            var bus = RabbitHutch.CreateBus($"host={rabbitMqHost};username={rabbitMqUser};password={rabbitMqPassword}");
+            var subscriber = new AutoSubscriber(bus, _subscriptionIdPrefix);
+            subscriber.SubscribeAsync(Assembly.GetExecutingAssembly());
+        }
+    }
+}

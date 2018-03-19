303ac48f8be08492dc35ada53c173817f1053e20
markslingerland <markslingerland@hotmail.com>
2018-03-19 11:12:01 +0100

    - oops mission abort

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+set -x # Show the output of the following commands (useful for debugging)
+chmod 600 rsa
+mv rsa ~/.ssh/rsa
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 rsa # Allow read access to the private key
-ssh-add rsa # Add the private key to SSH
+ssh-add ~/.ssh/rsa
+chmod 600 config.txt
+mv config.txt ~/.ssh/config
 
 git config --global push.default matching
 # git remote add deploy ssh://git@$IP:$PORT$DEPLOY_DIR

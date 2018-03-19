9ab0d1193999e4d3b8a502b9b446d986173ab555
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-19 23:29:15 +0100

    Fix build

--- Sombra.Messaging/IAsyncRequestHandler.cs
+++ Sombra.Messaging/IAsyncRequestHandler.cs
@@ -4,6 +4,7 @@ namespace Sombra.Messaging
 {
     public interface IAsyncRequestHandler<in TRequest, TResponse>
         where TRequest : IRequest<TResponse>
+        where TResponse : class
     {
         Task<TResponse> Handle(TRequest message);
     }

3d7a851e62bb908d39f4ba22ed0db4d6ef0f27fb
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-19 14:45:25 +0100

    Set bus lifetime to scoped

--- Sombra.Web/Startup.cs
+++ Sombra.Web/Startup.cs
@@ -20,7 +20,7 @@ namespace Sombra.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddTransient(c => CreateBus());
+            services.AddScoped(c => CreateBus());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

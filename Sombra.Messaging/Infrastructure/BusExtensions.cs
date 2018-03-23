3d385ae9fec2bea1b5614111bd15ad22e1adebe9
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-23 17:33:05 +0100

    Remove ConfigureAwait

--- Sombra.Messaging/Infrastructure/BusExtensions.cs
+++ Sombra.Messaging/Infrastructure/BusExtensions.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using EasyNetQ;
-using EasyNetQ.FluentConfiguration;
 
 namespace Sombra.Messaging.Infrastructure
 {
@@ -10,7 +8,7 @@ namespace Sombra.Messaging.Infrastructure
         public static async Task<TResponse> RequestAsync<TResponse>(this IBus bus, IRequest<TResponse> request)
             where TResponse : class, IResponse
         {
-            return await bus.RequestAsync<IRequest<TResponse>, TResponse>(request).ConfigureAwait(false);
+            return await bus.RequestAsync<IRequest<TResponse>, TResponse>(request);
         }
     }
 }

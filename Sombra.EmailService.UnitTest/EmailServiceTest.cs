896e75ca0fe7759dedb8e08f06cbe7f983ae864b
rubenev <0912723@hr.nl>
2018-03-26 20:47:28 +0200

    unittest folder replaced

---
+++ Sombra.EmailService.UnitTest/EmailServiceTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+
+namespace Sombra.EmailService.UnitTest
+{
+    [TestClass]
+    public class EmailServiceTest
+    {
+        //[TestMethod]
+        //public void Send()
+
+    }
+}

d40830f3f6d0b18a7e8289c45e16b4839d88f48f
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-26 09:27:45 +0200

    Return logs to UI

--- Sombra.Web/Areas/Development/Controllers/LoggingController.cs
+++ Sombra.Web/Areas/Development/Controllers/LoggingController.cs
@@ -1,9 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
 using EasyNetQ;
 using Microsoft.AspNetCore.Mvc;
-using Sombra.Messaging.Infrastructure;
 using Sombra.Messaging.Requests;
+using Sombra.Messaging.Responses;
 using Sombra.Web.Areas.Development.Models;
 
 namespace Sombra.Web.Areas.Development.Controllers
@@ -30,9 +31,10 @@ namespace Sombra.Web.Areas.Development.Controllers
         public async Task<IActionResult> Index(LogsQuery query)
         {
             var request = _mapper.Map<LogRequest>(query);
-            var logs = await _bus.RequestAsync(request);
+            var response = await _bus.RequestAsync<LogRequest, LogResponse>(request);
+            var logs = _mapper.Map<IEnumerable<LogViewModel>>(response.Logs);
 
-            return View();
+            return View("View", logs);
         }
     }
 }

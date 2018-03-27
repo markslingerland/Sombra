b8206b299d6a4e574973342fee59f65166e1a9b7
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-03-27 13:02:05 +0200

    Add some structure to Program

--- Sombra.EmailService/Program.cs
+++ Sombra.EmailService/Program.cs
@@ -1,26 +1,46 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
-using EasyNetQ;
-using EasyNetQ.AutoSubscribe;
+using Microsoft.Extensions.Configuration;
+using Sombra.Messaging.Infrastructure;
 
 namespace Sombra.EmailService
 {
     class Program
     {
-        private static string _subscriptionIdPrefix = "Sombra.EmailService";
+        private static string _rabbitMqConnectionString;
+
         static async Task Main(string[] args)
         {
             Console.WriteLine("EmailService started..");
 
-            var bus = RabbitHutch.CreateBus(Environment.GetEnvironmentVariable("RABBITMQ_CONNECTIONSTRING"));
-            var subscriber = new AutoSubscriber(bus, _subscriptionIdPrefix);
-            subscriber.SubscribeAsync(Assembly.GetExecutingAssembly());
+            // Laad de connectionstring in
+            SetupConfiguration();
 
-            while (true)
+            // Maak een bus aan, registreer alle handlers en start de autosubscriber en autoresponder
+            var serviceProvider = MessagingInstaller.Run(Assembly.GetExecutingAssembly(), _rabbitMqConnectionString);
+
+            // Laat de applicatie runnen
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void SetupConfiguration()
+        {
+            var isContainerized = Environment.GetEnvironmentVariable("CONTAINER_TYPE") != null;
+            if (isContainerized)
             {
-                Thread.Sleep(10000);
+                // Docker container. Haal de variabelen uit de environment (docker-compose-override)
+                _rabbitMqConnectionString = Environment.GetEnvironmentVariable("RABBITMQ_CONNECTIONSTRING");
+            }
+            else
+            {
+                // Geen container. Haal ze uit de appsettings.json
+                var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json").Build();
+
+                _rabbitMqConnectionString = config["RABBITMQ_CONNECTIONSTRING"];
             }
         }
     }

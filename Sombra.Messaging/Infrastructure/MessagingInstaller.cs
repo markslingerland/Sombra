3f1a16e90b76f1f90abe0968cdbb0364a64e4027
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-04-03 18:37:15 +0200

    Expand MessagingInstaller logging

--- Sombra.Messaging/Infrastructure/MessagingInstaller.cs
+++ Sombra.Messaging/Infrastructure/MessagingInstaller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 using EasyNetQ;
 using Microsoft.Extensions.DependencyInjection;
@@ -7,10 +8,11 @@ namespace Sombra.Messaging.Infrastructure
 {
     public static class MessagingInstaller
     {
-        public static ServiceProvider Run(Assembly assembly, string busConnectionString, Func<IServiceCollection, IServiceCollection> addAdditionalServices = null)
+        public static ServiceProvider Run(Assembly assembly, string busConnectionString, Func<IServiceCollection, IServiceCollection> addAdditionalServices = null, Action<ServiceProvider> connectionValidator = null)
         {
             addAdditionalServices = addAdditionalServices ?? (s => s);
             var bus = RabbitHutch.CreateBus(busConnectionString).WaitForConnection();
+            Console.WriteLine($"MessagingInstaller: Bus connected: {bus.IsConnected}.");
 
             var serviceProvider = addAdditionalServices(new ServiceCollection())
                 .AddEventHandlers(assembly)
@@ -18,11 +20,27 @@ namespace Sombra.Messaging.Infrastructure
                 .AddSingleton(bus)
                 .BuildServiceProvider(true);
 
+            Console.WriteLine($"MessagingInstaller: Services are registered.");
+
             var responder = new AutoResponder(bus, serviceProvider);
             responder.RespondAsync(assembly);
+            Console.WriteLine($"MessagingInstaller: AutoResponders initialized.");
 
             var subscriber = new CustomAutoSubscriber(bus, serviceProvider, assembly.FullName);
             subscriber.SubscribeAsync(assembly);
+            Console.WriteLine($"MessagingInstaller: AutoSubscribers initialized.");
+
+            if(connectionValidator != null)
+            {
+                var durationTracker = new Stopwatch();
+                durationTracker.Start();
+
+                Console.WriteLine($"MessagingInstaller: Running connectionValidator..");
+                connectionValidator(serviceProvider);
+                durationTracker.Stop();
+
+                Console.WriteLine($"MessagingInstaller: ConnectionValidator finished running in {durationTracker.ElapsedMilliseconds}ms.");
+            }
 
             return serviceProvider;
         }

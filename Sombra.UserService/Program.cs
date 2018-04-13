8431853ffab285b780248a53855c393312e0ec2a
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-04-13 12:54:42 +0200

    Add UserServiceProject

---
+++ Sombra.UserService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sombra.UserService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

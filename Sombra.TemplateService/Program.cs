21803cbd5d1e3db5de23c0fabfc4909dfbd96106
rubenev <0912723@hr.nl>
2018-04-13 13:31:02 +0200

    Start

---
+++ Sombra.TemplateService/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sombra.Messaging.Infrastructure;
+using System;
+using System.Reflection;
+using System.Threading;
+
+namespace Sombra.TemplateService
+{
+    class Program
+    {
+        private static string _rabbitMqConnectionString;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TemplateService started..");
+
+            SetupConfiguration();
+            var serviceProvider = MessagingInstaller.Run(
+                Assembly.GetExecutingAssembly(),
+                _rabbitMqConnectionString);
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        private static void SetupConfiguration()
+        {
+            var isContainerized = Environment.GetEnvironmentVariable("CONTAINER_TYPE") != null;
+            if (isContainerized)
+            {
+                _rabbitMqConnectionString = Environment.GetEnvironmentVariable("RABBITMQ_CONNECTIONSTRING");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}

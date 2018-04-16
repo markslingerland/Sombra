ae7495b6ea31a95d4751f2d96c48815183ee1962
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-04-16 12:22:36 +0200

    Add UserEmailExistsRequestHandlerTest

---
+++ Sombra.UserService.UnitTests/UserEmailExistsRequestHandlerTest.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sombra.Messaging.Events;
+using Sombra.Messaging.Requests;
+using Sombra.Messaging.Responses;
+using Sombra.UserService.DAL;
+
+namespace Sombra.UserService.UnitTests
+{
+    [TestClass]
+    public class UserEmailExistsRequestHandlerTest
+    {
+        [TestMethod]
+        public async Task Handle_EmailExists()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<UserContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                UserEmailExistsResponse response;
+                var request = new UserEmailExistsRequest
+                {
+                    EmailAddress = "john@doe.test"
+                };
+
+                using (var context = new UserContext(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Users.Add(new User
+                    {
+                        EmailAddress = "john@doe.test"
+                    });
+
+                    context.SaveChanges();
+                }
+
+                using (var context = new UserContext(options))
+                {
+                    var handler = new UserEmailExistsRequestHandler(context);
+                    response = await handler.Handle(request);
+                }
+
+                Assert.IsTrue(response.EmailExists);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        [TestMethod]
+        public async Task Handle_EmailNotExists()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+
+            try
+            {
+                var options = new DbContextOptionsBuilder<UserContext>()
+                    .UseSqlite(connection)
+                    .Options;
+
+                UserEmailExistsResponse response;
+                var request = new UserEmailExistsRequest
+                {
+                    EmailAddress = "ellen@doe.test"
+                };
+
+                using (var context = new UserContext(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Users.Add(new User
+                    {
+                        EmailAddress = "john@doe.test"
+                    });
+
+                    context.SaveChanges();
+                }
+
+                using (var context = new UserContext(options))
+                {
+                    var handler = new UserEmailExistsRequestHandler(context);
+                    response = await handler.Handle(request);
+                }
+
+                Assert.IsFalse(response.EmailExists);
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}

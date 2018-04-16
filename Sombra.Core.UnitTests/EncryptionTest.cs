1f2d310053c7e85831deef28e23543e8e9d16c34
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-04-16 15:08:21 +0200

    Add test for Encryption

---
+++ Sombra.Core.UnitTests/EncryptionTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sombra.Core.UnitTests
+{
+    [TestClass]
+    public class EncryptionTest
+    {
+        [TestMethod]
+        public void Encryption_ValidatePassword_Returns_True()
+        {
+            var password = "abc123";
+            var passwordToVerify = "abc123";
+            var encryptedPassword = Encryption.CreateHash(password);
+
+            var passwordIsCorrect = Encryption.ValidatePassword(passwordToVerify, encryptedPassword);
+            
+            Assert.IsTrue(passwordIsCorrect);
+        }
+
+        [TestMethod]
+        public void Encryption_ValidatePassword_Returns_False()
+        {
+            var password = "Abc123";
+            var passwordToVerify = "abc123";
+            var encryptedPassword = Encryption.CreateHash(password);
+
+            var passwordIsCorrect = Encryption.ValidatePassword(passwordToVerify, encryptedPassword);
+
+            Assert.IsFalse(passwordIsCorrect);
+        }
+
+        [TestMethod]
+        public void Encryption_CreateHash_ReturnsUnique()
+        {
+            var password = "abc123";
+
+            var encryptedPassword1 = Encryption.CreateHash(password);
+            var encryptedPassword2 = Encryption.CreateHash(password);
+
+            Assert.AreNotEqual(encryptedPassword1, encryptedPassword2);
+        }
+    }
+}

fca9cfe302c305d44f0329e6f300dd8ede99b7fc
rubenev <0912723@hr.nl>
2018-04-18 14:46:55 +0200

    removed unused elements

---
+++ Sombra.TemplateService/EmailTemplateService.cs
@@ -0,0 +1,34 @@
+﻿using Sombra.TemplateService.Templates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sombra.TemplateService
+{
+    public class Testclass
+    {
+        public class EmailTemplateService
+        {
+            private readonly Dictionary<string, string> _templateContent;
+            private readonly EmailType _type;
+
+            public EmailTemplateService(EmailTemplateRequest request) // + Context voeren
+            {
+                _type = request.EmailType;
+                _templateContent = request.TemplateContent;
+            }
+
+            public string Construct()
+            {
+                var template = string.Empty; // Haal je template uit je context aan de hand van je _type
+                foreach (var item in _templateContent)
+                {
+                    template = template.Replace($"[[{item.Key}]]", item.Value);
+                }
+
+                return template;
+            }
+        }
+    }
+}
+        

bd288f143db26739f01ec447d2d50db6591be7f8
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-04-30 22:00:26 +0200

    Add ActivateUserRequestHandler

---
+++ Sombra.Messaging/Requests/ActivateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿using Sombra.Messaging.Responses;
+
+namespace Sombra.Messaging.Requests
+{
+    public class ActivateUserRequest : Request<ActivateUserResponse>
+    {
+        public string ActivationToken { get; set; }
+    }
+}

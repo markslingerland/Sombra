27bc06c3ac5da45b187155cca834f0290a8ec053
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-04-30 22:01:00 +0200

    Add GetUserActivationTokenRequestHandler

---
+++ Sombra.IdentityService/GetUserActivationTokenRequestHandler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sombra.Core;
+using Sombra.Core.Enums;
+using Sombra.IdentityService.DAL;
+using Sombra.Messaging.Infrastructure;
+using Sombra.Messaging.Requests;
+using Sombra.Messaging.Responses;
+using CredentialType = Sombra.Core.Enums.CredentialType;
+
+namespace Sombra.IdentityService
+{
+    public class GetUserActivationTokenRequestHandler : IAsyncRequestHandler<GetUserActivationTokenRequest, GetUserActivationTokenResponse>
+    {
+        private readonly AuthenticationContext _context;
+
+        public GetUserActivationTokenRequestHandler(AuthenticationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetUserActivationTokenResponse> Handle(GetUserActivationTokenRequest message)
+        {
+            if (string.IsNullOrEmpty(message.EmailAddress))
+                return new GetUserActivationTokenResponse
+                {
+                    ErrorType = ErrorType.InvalidEmail
+                };
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Credentials.Any(c => c.Identifier.Equals(message.EmailAddress, StringComparison.OrdinalIgnoreCase) && c.CredentialType.Name == CredentialType.Email));
+
+            if (user != null)
+            {
+                if (user.IsActive)
+                    return new GetUserActivationTokenResponse
+                    {
+                        ErrorType = ErrorType.AlreadyActive
+                    };
+
+                user.ActivationToken = Hash.SHA256(Guid.NewGuid().ToString());
+                user.ActivationTokenExpirationDate = DateTime.UtcNow.AddDays(1);
+
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateException ex)
+                {
+                    ExtendedConsole.Log(ex);
+                    return new GetUserActivationTokenResponse();
+                }
+
+                return new GetUserActivationTokenResponse
+                {
+                    ActivationToken = user.ActivationToken
+                };
+            }
+
+            return new GetUserActivationTokenResponse
+            {
+                ErrorType = ErrorType.InvalidEmail
+            };
+        }
+    }
+}
---
+++ Sombra.Messaging/Responses/GetUserActivationTokenResponse.cs
@@ -0,0 +1,10 @@
+﻿using Sombra.Core.Enums;
+
+namespace Sombra.Messaging.Responses
+{
+    public class GetUserActivationTokenResponse : Response
+    {
+        public string ActivationToken { get; set; }
+        public ErrorType ErrorType { get; set; }
+    }
+}

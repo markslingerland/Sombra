d1737fa778a589c8020ca817f2fba84039abe92e
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-04-30 14:46:15 +0200

    Fix enum

--- Sombra.Web/Infrastructure/Authentication/UserManager.cs
+++ Sombra.Web/Infrastructure/Authentication/UserManager.cs
@@ -61,7 +61,7 @@ namespace Sombra.Web.Infrastructure.Authentication
             var forgotPasswordResponse = await _bus.RequestAsync(forgotPasswordRequest);
             var actionurl = $"{_httpContext.Request.Host}/Account/ChangePassword/{forgotPasswordResponse.Secret}";
 
-            var emailTemplateRequest = new EmailTemplateRequest(EmailType.ForgotPasswordTemplate, TemplateContentBuilder.CreateForgotPasswordTempleteContent(name, actionurl, clientInfo.OperatingSystem, clientInfo.BrowserName));
+            var emailTemplateRequest = new EmailTemplateRequest(EmailType.ForgotPassword, TemplateContentBuilder.CreateForgotPasswordTempleteContent(name, actionurl, clientInfo.OperatingSystem, clientInfo.BrowserName));
             var response = await _bus.RequestAsync(emailTemplateRequest);
 
             var email = new EmailEvent(new EmailAddress("noreply", "noreply@ikdoneer.nu"), new EmailAddress(name, forgotPasswordViewModel.EmailAdress), "Wachtwoord vergeten ikdoneer.nu",

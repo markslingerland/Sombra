bf7b258a40f81d6a9ba1f3a89b1f322c42296365
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-07 23:14:54 +0200

    Add ping

---
+++ Sombra.Messaging/DependencyValidation/PingAutoResponder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using EasyNetQ;
+using Sombra.Messaging.Infrastructure;
+
+namespace Sombra.Messaging.DependencyValidation
+{
+    public class PingAutoResponder : AutoResponder
+    {
+        public PingAutoResponder(IBus bus, IAutoResponderRequestDispatcher messageDispatcher) : base(bus, messageDispatcher)
+        {
+        }
+
+        public override void RespondAsync(params Type[] responderTypes)
+        {
+            var genericBusRepondMethod = GetRespondMethodOfBus(nameof(Bus.RespondAsync), typeof(Func<,>));
+            var responderInfos = GetResponderInfos(responderTypes, typeof(IAsyncRequestHandler<,>));
+            // ResponderInfos wrappen
+
+
+            Type ResponderTypeFromRequestTypeDelegate(Type messageType, Type responseType) => typeof(Func<,>).MakeGenericType(messageType, typeof(Task<>).MakeGenericType(responseType));
+            InvokeMethods(responderInfos, nameof(AutoResponderMessageDispatcher.DispatchAsync), genericBusRepondMethod, ResponderTypeFromRequestTypeDelegate);
+        }
+    }
+}
---
+++ Sombra.Messaging/DependencyValidation/PingRequestHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using EasyNetQ;
+using Sombra.Messaging.Infrastructure;
+using Sombra.Messaging.Requests;
+using Sombra.Messaging.Responses;
+
+namespace Sombra.Messaging.DependencyValidation
+{
+    public class GenericPingRequestHandler<TRequest, TResponse, TInnerResponse> : IAsyncRequestHandler<TRequest, TResponse>
+        where TRequest : class, IRequest<TResponse>
+        where TResponse : PingResponse<TInnerResponse>, new()
+        where TInnerResponse : class, IResponse
+    {
+        public async Task<TResponse> Handle(TRequest message)
+        {
+            return new TResponse
+            {
+                IsOnline = true
+            };
+        }
+    }
+
+    public class PingRequestHandler : IAsyncRequestHandler<Ping<EmailTemplateRequest, EmailTemplateResponse>, PingResponse<EmailTemplateResponse>>
+    {
+        public async Task<PingResponse<EmailTemplateResponse>> Handle(Ping<EmailTemplateRequest, EmailTemplateResponse> message)
+        {
+            return new PingResponse<EmailTemplateResponse>
+            {
+                IsOnline = true
+            };
+        }
+    }
+
+    public class abc
+    {
+        public abc(IBus bus)
+        {
+            var ping = new Ping<EmailTemplateRequest, EmailTemplateResponse>();
+            var response = bus.RequestAsync(ping);
+
+            var handler = new GenericPingRequestHandler<Ping<EmailTemplateRequest, EmailTemplateResponse>, PingResponse<EmailTemplateResponse>, EmailTemplateResponse>();
+
+
+            var result = handler.Handle(ping);
+        }
+    }
+}

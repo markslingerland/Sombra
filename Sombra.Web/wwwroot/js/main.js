898cb8a84874be93a7c5f5c2a51a17562ae5be19
JaehaeryStef <stefdekramer@hotmail.com>
2018-05-07 12:56:30 +0200

    added css and javascript

---
+++ Sombra.Web/wwwroot/js/main.js
@@ -0,0 +1,7 @@
+$("#header-button").click(function () {
+    $(".header-dropdown-menu").css('height', '100%');
+});
+
+$("#header-close-dropdown").click(function () {
+    $(".header-dropdown-menu").css('height', '0px');
+});

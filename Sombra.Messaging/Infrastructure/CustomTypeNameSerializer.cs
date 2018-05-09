84ef71a10e784b885df2d91c870ee166520a47fa
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-09 12:28:23 +0200

    Fix CustomTypeNameSerializer

--- Sombra.Messaging/Infrastructure/CustomTypeNameSerializer.cs
+++ Sombra.Messaging/Infrastructure/CustomTypeNameSerializer.cs
@@ -8,25 +8,29 @@ namespace Sombra.Messaging.Infrastructure
     public class CustomTypeNameSerializer : ITypeNameSerializer
     {
         private readonly string _messagesAssemblyName;
-        private readonly string _messagesNamespacePrefix;
+        private readonly string __messagesAssemblySubstitute = "[a]";
         private readonly ConcurrentDictionary<string, Type> _deserializedTypes = new ConcurrentDictionary<string, Type>();
         private readonly ConcurrentDictionary<Type, string> _serializedTypes = new ConcurrentDictionary<Type, string>();
 
         public CustomTypeNameSerializer()
         {
-            var baseMessageType = typeof(Message);
-            _messagesAssemblyName = baseMessageType.Assembly.GetName().Name;
-            _messagesNamespacePrefix = $"{baseMessageType.Namespace}.";
+            _messagesAssemblyName = typeof(Message).Assembly.GetName().Name;
         }
 
         public Type DeSerialize(string typeName)
         {
             return _deserializedTypes.GetOrAdd(typeName, t =>
             {
-                var type = Type.GetType($"{_messagesNamespacePrefix}{t}, {_messagesNamespacePrefix}");
+                var type = Type.GetType($"{t.Replace(__messagesAssemblySubstitute, _messagesAssemblyName)}, {_messagesAssemblyName}");
                 if (type == null)
                 {
-                    throw new EasyNetQException("Cannot find type {0}", t);
+                    var nameParts = t.Split(':');
+                    if (nameParts.Length != 2)
+                        throw new EasyNetQException("type name {0}, is not a valid EasyNetQ type name. Expected Type:Assembly", t);
+
+                    type = Type.GetType(nameParts[0] + ", " + nameParts[1]);
+                    if (type == null)
+                        throw new EasyNetQException("Cannot find type {0}", t);
                 }
                 return type;
             });
@@ -36,13 +40,10 @@ namespace Sombra.Messaging.Infrastructure
         {
             return _serializedTypes.GetOrAdd(type, t =>
             {
-                if (!t.FullName.StartsWith(_messagesNamespacePrefix))
-                    throw new EasyNetQException($"The type must be in {_messagesNamespacePrefix}.");
+                var typeName = t.FullName + ":" + t.GetTypeInfo().Assembly.GetName().Name;
+                if (t.Assembly.GetName().Name == _messagesAssemblyName)
+                    typeName = t.FullName.Replace(_messagesAssemblyName, __messagesAssemblySubstitute);
 
-                if (t.Assembly.GetName().Name != _messagesAssemblyName)
-                    throw new EasyNetQException($"The type must be in {_messagesAssemblyName}.");
-
-                var typeName = t.FullName.Replace(_messagesNamespacePrefix, "");
                 if (typeName.Length > 255)
                     throw new EasyNetQException("The serialized name of type '{0}' exceeds the AMQP " +
                                                 "maximum short string length of 255 characters.", t.Name);

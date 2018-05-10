8a246e966413ee3a15e8e860e9399728cbef61a2
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-10 17:15:58 +0200

    Add program.cs and context

---
+++ Sombra.DonateService/DAL/DonationsContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sombra.Infrastructure.DAL;
+
+namespace Sombra.DonateService.DAL
+{
+    public class DonationsContext : SombraContext<DonationsContext>
+    {
+        public DonationsContext() { }
+
+        public DonationsContext(DbContextOptions<DonationsContext> options) : base(options) { }
+    }
+}

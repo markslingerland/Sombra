624b8e437c68ddc87ef8f808bf1bdd594f412ce4
rubenev <0912723@hr.nl>
2018-05-14 15:24:36 +0200

    merge with master

--- Sombra.CharityActionService/GetCharityActionByKeyRequestHandler.cs
+++ Sombra.CharityActionService/GetCharityActionByKeyRequestHandler.cs
@@ -4,29 +4,26 @@ using Sombra.CharityActionService.DAL;
 using Sombra.Core;
 using Sombra.Messaging.Requests;
 using Sombra.Messaging.Responses;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Sombra.CharityActionService
 {
     public class GetCharityActionByKeyRequestHandler
     {
-        private readonly CharityActionContext _charityeContext;
+        private readonly CharityActionContext _charityActionContext;
         private readonly IMapper _mapper;
 
-        public GetCharityActionByKeyRequestHandler(CharityActionContext charityContext, IMapper mapper)
+        public GetCharityActionByKeyRequestHandler(CharityActionContext charityActionContext, IMapper mapper)
         {
-            _charityeContext = charityContext;
+            _charityActionContext = charityActionContext;
             _mapper = mapper;
         }
 
         public async Task<GetCharityActionResponse> Handle(GetCharityActionRequest message)
         {
             ExtendedConsole.Log("GetCharityActionByKeyRequestHandler received");
-            var charityAction = await _charityeContext.CharityActions.Where(b => b.CharityActionkey.Equals(message.CharityActionkey)).Select(a => a).FirstOrDefaultAsync();
+            var charityAction = await _charityActionContext.CharityActions.Where(b => b.CharityActionkey.Equals(message.CharityActionkey)).Select(a => a).FirstOrDefaultAsync();
             return charityAction != null ? _mapper.Map<GetCharityActionResponse>(charityAction) : new GetCharityActionResponse();
         }
     }

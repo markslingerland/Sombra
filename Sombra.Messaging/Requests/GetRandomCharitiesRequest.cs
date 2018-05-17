8f3fa3bed78cdd141d6072da24920bc2154b7650
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-17 21:18:18 +0200

    Add GetRandomCharitiesRequest(Handler)

---
+++ Sombra.Messaging/Requests/GetRandomCharitiesRequest.cs
@@ -0,0 +1,9 @@
+﻿using Sombra.Messaging.Responses;
+
+namespace Sombra.Messaging.Requests
+{
+    public class GetRandomCharitiesRequest : Request<GetRandomCharitiesResponse>
+    {
+        public int Amount { get; set; }
+    }
+}

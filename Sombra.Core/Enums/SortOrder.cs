4c90ae2b6012eee0e099fb1221d683422a34617a
Jelle Kerkstra <j.kerkstra@viscon.eu>
2018-05-17 15:47:52 +0200

    Add queryable extensions

---
+++ Sombra.Core/Enums/SortOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sombra.Core.Enums
+{
+    public enum SortOrder
+    {
+        Asc = 0,
+        Desc = 1
+    }
+}
---
+++ Sombra.Infrastructure/DAL/QueryableExtensions.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System.Text;
+using Core.Enums;
+
+namespace Sombra.Infrastructure.DAL
+{
+    public static class QueryableExtensions
+    {
+        public static IQueryable<T> ApplyPagination<T>(this IOrderedQueryable<T> queryable, int pageNumber, int pageSize)
+            where T : class
+        {
+            return queryable.Skip((pageNumber - 1) * pageSize).Take(pageSize);
+        }
+
+        public static async Task<IEnumerable<TDestination>> ProjectToPagedListAsync<TDestination, TSource>(this IOrderedQueryable<TSource> queryable, int pageNumber, int pageSize, IConfigurationProvider mapperConfiguration)
+            where TSource : class
+            where TDestination : class
+        {
+            return await queryable.ApplyPagination(pageNumber, pageSize).ProjectToListAsync<TDestination>(mapperConfiguration);
+        }
+
+        public static async Task<IEnumerable<TDestination>> ProjectToPagedListAsync<TDestination, TKey, TSource>(this IQueryable<TSource> queryable, Expression<Func<TSource, TKey>> keySelector, SortOrder sortOrder, int pageNumber, int pageSize, IConfigurationProvider mapperConfiguration)
+            where TSource : class
+            where TDestination : class
+        {
+            var orderedQueryable = sortOrder == sortOrder.Asc
+                ? queryable.OrderBy(keySelector)
+                : queryable.OrderByDescending(keySelector);
+
+            return await orderedQueryable.ProjectToPagedListAsync<TDestination, TSource>(pageNumber, pageSize, mapperConfiguration);
+        }
+    }
+}

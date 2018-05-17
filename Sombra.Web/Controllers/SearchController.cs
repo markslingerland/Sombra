268ea1a2fd80a2a153c089de2da4dc46b000ad50
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-17 21:46:52 +0200

    Implement featured charities

--- Sombra.Web/Controllers/SearchController.cs
+++ Sombra.Web/Controllers/SearchController.cs
@@ -1,26 +1,42 @@
 ﻿using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Sombra.Messaging.Requests;
+using Sombra.Web.Infrastructure.Messaging;
 using Sombra.Web.ViewModels.Search;
 
 namespace Sombra.Web.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly ICachingBus _bus;
+        private readonly IMapper _mapper;
+
+        public SearchController(ICachingBus bus, IMapper mapper)
+        {
+            _bus = bus;
+            _mapper = mapper;
+        }
+
         [HttpGet]
         public async Task<IActionResult> GetTopCharities(TopCharitiesQuery query)
         {
-            var result = new TopCharitiesViewModel
-            {
-                Charities = new List<CharityItemViewModel>
-                {
-                    new CharityItemViewModel(),
-                    new CharityItemViewModel(),
-                    new CharityItemViewModel()
-                }
-            };
+            var request = _mapper.Map<GetRandomCharitiesRequest>(query);
+            var response = await _bus.RequestAsync(request);
 
-            return PartialView("_ActionItemsWrapper", result);
+            if (response.IsRequestSuccessful)
+            {
+                var model = new TopCharitiesViewModel
+                {
+                    Charities = _mapper.Map<List<CharityItemViewModel>>(response.Results)
+                };
+
+                return PartialView("_ActionItemsWrapper", model);
+            }
+
+            return new StatusCodeResult((int) HttpStatusCode.ServiceUnavailable);
         }
     }
 }

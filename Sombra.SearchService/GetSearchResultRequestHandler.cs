8d65433d0c4cf63eeb03d8b3caa8258bae75b9a3
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-21 18:58:26 +0200

    Remove testing code

--- Sombra.SearchService/GetSearchResultRequestHandler.cs
+++ Sombra.SearchService/GetSearchResultRequestHandler.cs
@@ -9,7 +9,6 @@ using System.Linq.Expressions;
 using System;
 using Sombra.Core.Extensions;
 using Sombra.Infrastructure.DAL;
-using EasyNetQ;
 
 namespace Sombra.SearchService
 {
@@ -32,17 +31,12 @@ namespace Sombra.SearchService
             if (!string.IsNullOrEmpty(message.Keyword)) filter = filter.And(l => l.Name.Contains(message.Keyword));
             if (!message.SearchContentType.Equals(Core.Enums.SearchContentType.Default)) filter = filter.And(l => message.SearchContentType.Equals(l.Type));
 
-            var response =  new GetSearchResultResponse();
-            
-            response.TotalResult = _context.Content.Count(filter);
-            var debug = _context.Content.Where(filter);
-            var debug1 = debug.OrderBy(c => c.Name, message.SortOrder);
-            var debug2 = await debug1.ProjectToPagedListAsync<SearchResult>(message.PageNumber, message.PageSize, _mapper);
-
-
-            response.Results = await _context.Content.Where(filter).OrderBy(c => c.Name, message.SortOrder).ProjectToPagedListAsync<SearchResult>(message.PageNumber, message.PageSize, _mapper);
-            
-            return response;
+            return new GetSearchResultResponse
+            {
+                TotalResult = _context.Content.Count(filter),
+                Results = await _context.Content.Where(filter).OrderBy(c => c.Name, message.SortOrder)
+                    .ProjectToPagedListAsync<SearchResult>(message.PageNumber, message.PageSize, _mapper)
+            };
         }
     }
 }

95bc21b0a5c667f47afa106f32ba93814a2babd2
rubenev <0912723@hr.nl>
2018-05-23 15:33:51 +0200

    Start charity page

---
+++ Sombra.Web/wwwroot/js/charity.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $('#top-charities').load('@Url.Action("GetTopCharities", "Search", new { Amount = 3})');
+});
+$(document).ready(function () {
+    ;
+    var str = '<%= ViewData["CharityImage"] %>';
+    $("#svg-charity").attr("xlink:href", str);
+});

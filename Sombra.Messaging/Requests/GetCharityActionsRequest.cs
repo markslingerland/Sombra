027c3e16fc3464e98e3d1ba47f8d5f6dfc88f8bc
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-28 21:23:18 +0200

    Add GetCharityActionsRequestHandlerTests

--- Sombra.Messaging/Requests/GetCharityActionsRequest.cs
+++ Sombra.Messaging/Requests/GetCharityActionsRequest.cs
@@ -3,8 +3,16 @@ using Sombra.Messaging.Responses;
 
 namespace Sombra.Messaging.Requests
 {
+    [Cachable]
     public class GetCharityActionsRequest : Request<GetCharityActionsResponse>
     {
+        [CacheKey]
         public Guid CharityKey { get; set; }
+
+        [CacheKey]
+        public int PageSize { get; set; }
+
+        [CacheKey]
+        public int PageNumber { get; set; }
     }
 }
--- Sombra.Messaging/Responses/GetCharityActionsResponse.cs
+++ Sombra.Messaging/Responses/GetCharityActionsResponse.cs
@@ -1,7 +1,11 @@
-﻿namespace Sombra.Messaging.Responses
+﻿using System.Collections.Generic;
+using Sombra.Messaging.Shared;
+
+namespace Sombra.Messaging.Responses
 {
     public class GetCharityActionsResponse : Response
     {
-
+        public List<CharityAction> Results { get; set; }
+        public int TotalResult { get; set; }
     }
 }

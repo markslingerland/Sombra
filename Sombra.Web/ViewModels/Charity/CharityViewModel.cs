60970e008df486ced38ba4ea31b7a9b71de50578
rubenev <0912723@hr.nl>
2018-05-29 23:30:38 +0200

    added models

---
+++ Sombra.Web/ViewModels/Charity/CharityViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sombra.Web.ViewModels.Charity
+{
+    public class CharityViewModel
+    {
+        public CharityActionModel charityAction { get; set; }
+        public CharityModel charity { get; set; }
+        public DonationsToCharityModel donationsToCharity { get; set; }
+    }
+}

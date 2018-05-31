8b4dbc7ef46a06e13492602dba551ef21e7982c6
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-05-31 20:49:27 +0200

    Add GetCharityTotalRequest

---
+++ Sombra.DonateService/GetCharityTotalRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Sombra.DonateService.DAL;
+using Sombra.Messaging.Infrastructure;
+using Sombra.Messaging.Requests.Donate;
+using Sombra.Messaging.Responses.Donate;
+
+namespace Sombra.DonateService
+{
+    public class GetCharityTotalRequestHandler : IAsyncRequestHandler<GetCharityTotalRequest, GetCharityTotalResponse>
+    {
+        private readonly DonationsContext _context;
+
+        public GetCharityTotalRequestHandler(DonationsContext context)
+        {
+            _context = context;
+        }
+
+        public Task<GetCharityTotalResponse> Handle(GetCharityTotalRequest message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sombra.Messaging/Requests/Donate/GetCharityTotalRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Sombra.Messaging.Responses.Donate;
+
+namespace Sombra.Messaging.Requests.Donate
+{
+    public class GetCharityTotalRequest : Request<GetCharityTotalResponse>
+    {
+        public Guid CharityKey { get; set; }
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+    }
+}

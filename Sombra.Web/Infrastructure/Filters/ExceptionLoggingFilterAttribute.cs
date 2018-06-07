b691251fb97b667151bbf8138bf1e68943b37de1
Jelle Kerkstra <j.kerkstra@viscon.eu>
2018-06-07 17:12:56 +0200

    Add ExceptionLoggingFilterAttribute

---
+++ Sombra.Web/Infrastructure/Filters/ExceptionLoggingFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using Sombra.Web.Infrastructure.Messaging;
+
+namespace Sombra.Web.Infrastructure.Filters
+{
+    public class ExceptionLoggingFilterAttribute : ExceptionFilterAttribute
+    {
+        private readonly IBus _bus;
+
+        public ExceptionLoggingFilterAttribute(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public override async Task OnExceptionAsync(ExceptionContext context)
+        {
+            ExtendedConsole.Log(ex);
+            await _bus.SendAsync(ServiceInstaller.ExceptionQueue, new ExceptionMessage
+            {
+                Exception = ex
+            });
+
+            await base.OnExceptionAsync(context);
+        }
+    }
+}

8d513933e17c33508e767a63c176cc465a130e65
markslingerland <markslingerland@hotmail.com>
2018-06-08 14:46:09 +0200

    - Added missing properties

--- Sombra.Messaging/Shared/CharityAction.cs
+++ Sombra.Messaging/Shared/CharityAction.cs
@@ -15,6 +15,7 @@ namespace Sombra.Messaging.Shared
         public string Name { get; set; }
         public string Description { get; set; }
         public string CoverImage { get; set; }
+        public string ThankYou { get; set; }
 
         public Guid OrganiserUserKey { get; set; }
         public string OrganiserImage { get; set; }

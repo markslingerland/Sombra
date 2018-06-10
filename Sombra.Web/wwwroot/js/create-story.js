663db639409a81e6e1d228856af5b2eab38c801a
rubenev <0912723@hr.nl>
2018-06-10 14:21:10 +0200

    adding images js

--- Sombra.Web/wwwroot/js/create-story.js
+++ Sombra.Web/wwwroot/js/create-story.js
@@ -1,21 +1,26 @@
-﻿var $carousel = $('.main-carousel').flickity({
- cellAlign: 'left',
-    contain: true,
-    prevNextButtons: false,
-    pageDots: false
-      });
-
- // Flickity instance
-var flkty = $carousel.data('flickity');
-// elements
-var $cellButtonGroup = $('.button-group--cells');
-var $cellButtons = $cellButtonGroup.find('.button');
-
-// previous
-$('.button--previous').on('click', function () {
-    $carousel.flickity('previous');
-    });
-// next
-$('.button--next').on('click', function () {
-    $carousel.flickity('next');
+﻿$("document").ready(function () {
+    document.getElementById('file').addEventListener('change', mainPic, true);
+    document.getElementById('file-header').addEventListener('change', headerPic, true);
+    function mainPic() {
+        var file = document.getElementById("file").files[0];
+        var reader = new FileReader();
+        reader.onloadend = function () {
+            document.getElementById('story-image').style.backgroundImage = "url(" + reader.result + ")";
+        };
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+        }
+    }
+    function headerPic() {
+        var file = document.getElementById("file-header").files[0];
+        var reader = new FileReader();
+        reader.onloadend = function () {
+            document.getElementById('header-image').style.backgroundImage = "url(" + reader.result + ")";
+        };
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+        }
+    }
 });

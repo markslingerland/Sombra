9f61f3fa01b74d60b57fa6c81cd35f37300d8f79
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-06-10 15:37:48 +0200

    Add user

--- Sombra.Messaging/Responses/User/GetUserByKeyResponse.cs
+++ Sombra.Messaging/Responses/User/GetUserByKeyResponse.cs
@@ -1,21 +1,8 @@
-﻿using System;
-
-namespace Sombra.Messaging.Responses.User
+﻿namespace Sombra.Messaging.Responses.User
 {
     public class GetUserByKeyResponse : Response
     {
         public bool UserExists { get; set; }
-        public Guid UserKey { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string AddressLine1 { get; set; }
-        public string AddressLine2 { get; set; }
-        public string ZipCode { get; set; }
-        public string City { get; set; }
-        public string Country { get; set; }
-        public string EmailAddress { get; set; }
-        public string PhoneNumber { get; set; }
-        public DateTime BirthDate { get; set; }
-        public string ProfileImage { get; set; }
+        public Shared.User User { get; set; }
     }
 }

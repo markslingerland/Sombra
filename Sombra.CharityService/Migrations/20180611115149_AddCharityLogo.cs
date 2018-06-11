45000c65bcfe286fb1114597b23d7a8ac3120644
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-06-11 13:52:02 +0200

    Add CharityLogo

---
+++ Sombra.CharityService/Migrations/20180611115149_AddCharityLogo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sombra.CharityService.Migrations
+{
+    public partial class AddCharityLogo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Logo",
+                table: "Charities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Logo",
+                table: "Charities");
+        }
+    }
+}

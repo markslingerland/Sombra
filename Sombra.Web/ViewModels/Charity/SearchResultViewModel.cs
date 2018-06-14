b249a89da98bcce66db72652b0f3c91c46c8e505
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-06-14 16:50:07 +0200

    Fix pagination

---
+++ Sombra.Web/ViewModels/Charity/SearchResultViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Sombra.Core.Enums;
+
+namespace Sombra.Web.ViewModels.Charity
+{
+    public class SearchResultViewModel
+    {
+
+        public string Slogan { get; set; }
+        public Category Category { get; set; }
+        public string CoverImage { get; set; }
+        public string Url { get; set; }
+        public string Logo { get; set; }
+    }
+}
---
+++ Sombra.Web/ViewModels/Charity/SearchResultsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Sombra.Web.ViewModels.Charity
+{
+    public class SearchResultsViewModel
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalNumberOfResults { get; set; }
+        public List<SearchResultViewModel> Results { get; set; }
+    }
+}

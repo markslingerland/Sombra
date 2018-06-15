0ce0e90369a2323369b8ae6a3b46fb7db2f6ccd4
Stef <stefdekramer3@gmail.com>
2018-06-15 13:12:19 +0200

    form changes

--- Sombra.Web/wwwroot/js/flow-3/charity-sign-up.js
+++ Sombra.Web/wwwroot/js/flow-3/charity-sign-up.js
@@ -18,6 +18,21 @@ $('#control_02').click(function () {
     }
 });
 
+
+$('#charity').click(function () {
+    if ($('#charity').is(':checked')) {
+        $("#name-charity").attr("placeholder", "Naam van het goede doel");
+        $(".category-dropdown p").text("Selecteer categorieën voor het goede doel…");
+    }
+});
+
+$('#stichting').click(function () {
+    if ($('#stichting').is(':checked')) {
+        $("#name-charity").attr("placeholder", "Naam van de stichting");
+        $(".category-dropdown p").text("Selecteer categorieën voor de stichting…");
+    }
+});
+
 $('input:radio[name="pay-time"]').click(function (){
     $(".section-1 .next-step-holder").css("display", "inline-block");
 })
@@ -65,35 +80,130 @@ $('#one-time').on('input', function () {
 // Navigation
 
 $('#button-toSection-2').click(function(){
+
+    let namecharity = $('#name-charity').is(':filled');
+    let kvknumber = $('#kvk-number').is(':filled');
+    let firstNumbers = $('#first-numbers').is(':filled');
+    let lastNumbers = $('#last-numbers').is(':filled');
+    let cardholder = $('#card-holder').is(':filled');
+    let nameContact = $('#name-contact').is(':filled');
+    let email = $('#email').is(':filled');
+    let password = $('#password').is(':filled');
+    let passwordrepeat = $('#password-repeat').is(':filled');
+    
+    if (namecharity == false) {
+        $('#name-charity').addClass("not-valid");
+       } else {
+        $('#name-charity').removeClass("not-valid");
+       }
+    
+       if (kvknumber == false) {
+        $('#kvk-number').addClass("not-valid");
+       } else {
+        $('#kvk-number').removeClass("not-valid");
+       }
+       
+       if (firstNumbers == false) {
+        $('#first-numbers').addClass("not-valid");
+       } else {
+        $('#first-numbers').removeClass("not-valid");
+       }
+
+       if (lastNumbers == false) {
+        $('#last-numbers').addClass("not-valid");
+       } else {
+        $('#last-numbers').removeClass("not-valid");
+       }
+
+       if (cardholder == false) {
+        $('#card-holder').addClass("not-valid");
+       } else {
+        $('#card-holder').removeClass("not-valid");
+       }
+
+       if (nameContact == false) {
+        $('#name-contact').addClass("not-valid");
+       } else {
+        $('#name-contact').removeClass("not-valid");
+       }
+
+       if (email == false) {
+        $('#email').addClass("not-valid");
+       } else {
+        $('#email').removeClass("not-valid");
+       }
+
+       if (password == false) {
+        $('#password').addClass("not-valid");
+       } else {
+        $('#password').removeClass("not-valid");
+       }
+
+
+       if (passwordrepeat == false) {
+        $('#password-repeat').addClass("not-valid");
+       } else {
+        $('#password-repeat').removeClass("not-valid");
+       }
+
+       if (iban_bankChecker && choosePaymentOption && lastNameFilled && postcodeFilled && houseNumberFilled || $('#ideal').is(':checked') && iban_bankChecker || $('#paypall').is(':checked')) {
+        $(".section-2").hide();
+        $(".section-3").show();
+    }
+    
+
+
     $('.section-01').hide();
     $('.section-02').show();
-    event.preventDefault();
+    $('.section-03').hide();
+});
+
+$('#button-toSection-3').click(function(){
+    $('.section-01').hide();
+    $('.section-02').hide();
+    $('.section-03').show();
 });
 
 $(".first-section").click(function(){
-    $(".section-1").show();
-    $(".section-2").hide();
-    $(".section-3").hide();
-    $(".section-4").hide();
+    $(".section-01").show();
+    $(".section-02").hide();
+    $(".section-03").hide();
 });
 
 $(".second-section").click(function(){
-    $(".section-2").show();
-    $(".section-1").hide();
-    $(".section-3").hide();
-    $(".section-4").hide();
+    $(".section-02").show();
+    $(".section-01").hide();
+    $(".section-03").hide();
 });
 
 $(".third-section").click(function(){
-    $(".section-3").show();
-    $(".section-1").hide();
-    $(".section-2").hide();
-    $(".section-4").hide();
+    $(".section-03").show();
+    $(".section-01").hide();
+    $(".section-02").hide();
 });
 
-$(".fourth-section").click(function(){
-    $(".section-4").show();
-    $(".section-1").hide();
-    $(".section-2").hide();
-    $(".section-3").hide();
+$(".section-02 #go-back").click(function(){
+    $(".section-03").hide();
+    $(".section-01").show();
+    $(".section-02").hide();
+});
+
+$('.category-dropdown').click(function(){
+    if ($('.checkbox-dropdown').hasClass("shown")) {
+     $('.checkbox-dropdown').hide();
+     $('.checkbox-dropdown').removeClass("shown");
+     $('.category-dropdown').removeClass("category-dropdown-clicked");
+ 
+    } else {
+     $('.checkbox-dropdown').show();
+     $('.checkbox-dropdown').addClass("shown");
+     $('.category-dropdown').addClass("category-dropdown-clicked");
+    }
+ });
+
+ var limit = 3;
+$('input.single-checkbox').on('change', function(evt) {
+   if($(this).siblings(':checked').length >= limit) {
+       this.checked = false;
+   }
 });

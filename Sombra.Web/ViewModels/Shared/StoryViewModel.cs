9e0c4c049d0e84d6f53986eaf09cc37476997861
Jelle Kerkstra <jellekerkstra@hotmail.com>
2018-06-16 16:39:55 +0200

    Fix homepage styling

--- Sombra.Web/ViewModels/Shared/StoryViewModel.cs
+++ Sombra.Web/ViewModels/Shared/StoryViewModel.cs
@@ -5,7 +5,7 @@
         public string AuthorName { get; set; }
         public string UrlComponent { get; set; }
         public string CoverImage { get; set; }
-        public string Title { get; set; }
+        public string QuoteText { get; set; }
         public string CharityUrl { get; set; }
     }
 }

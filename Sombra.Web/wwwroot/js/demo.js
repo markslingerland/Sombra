df19689124a324bcea628bf7bf67ae0231bcfb75
markslingerland <markslingerland@hotmail.com>
2018-06-22 13:43:49 +0200

    - Added Demo yet again

---
+++ Sombra.Web/wwwroot/js/demo.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#name-charity').val('Tear');
+    $('#kvk-number').val('12345678');
+    $('#kvk-number').val('12345678');
+    $('#first-numbers').val('01');
+    $('#kvk-number').val('12345678');
+    $('#last-numbers').val('0123456789');
+    $('#name-contact').val('Lars ter Kraaij');
+    $('#card-holder').val('L. ter Kraaij');
+    $('#email').val('lars@email.com');
+    $('#password').val('passwordLars');
+    $('#password-repeat').val('passwordLars');    
+
+    var bankSelect = $('#select-your-bank');
+    bankSelect[0].selectize.setValue(1, false);
+
+    $('.category-checkbox-holder').find('label[data-category-id="4"] input[type="checkbox"]').prop('checked', true);
+    CategoryChanged();
+});
